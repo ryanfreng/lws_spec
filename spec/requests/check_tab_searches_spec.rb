0a8afb0f5a376cc7b5f0424a1438ee7492694c16
Ryan Freng <ryanfreng@gmail.com>
2012-07-31 22:57:07 -0500

    adds basic check for articles tab

--- spec/requests/check_tab_searches_spec.rb
+++ spec/requests/check_tab_searches_spec.rb
@@ -8,5 +8,25 @@ feature "Should have current year as copyright" do
     page.should have_content '©'
     find("#copyright").should have_content Time.new().strftime("%Y")
   end
+
+end
+
+feature "Should have search tabs" do
+  before { visit '/' }
+
+  scenario "check for articles tab" do
+    click_link 'Articles'
+    page.should have_content 'Search 3 general databases simultaneously'
+  end
+
+  scenario "check for books tab" do
+  end
+  scenario "check for journals tab" do
+  end
+  scenario "check for databases tab" do
+  end
+  scenario "check for website tab" do
+  end
+
 end
 
